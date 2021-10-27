cdf6f2906a34c382bd0a3309ca02f54ba414ead7
liav2002 <liavmail00@gmail.com>
2021-10-27 16:05:32 +0300

    Initial cod - Welcome functions.

---
+++ Targil0/Program0871.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome0871();
+        }
+
+        static partial void Welcome3539();
+        private static void Welcome0871()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application.\n", name);
+            Console.ReadKey();
+        }
+    }
+}
